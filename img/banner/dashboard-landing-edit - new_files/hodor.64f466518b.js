d9886bae080f67cc793fd2d92c6d84f22d5afe97
iyiola <iyiolaosuagwu1@gmail.com>
2020-02-29 01:19:17 +0100

    new changes and also adde d new assets

---
+++ img/banner/dashboard-landing-edit - new_files/hodor.64f466518b.js
@@ -0,0 +1,2 @@
+webpackJsonp([27],{1599:function(module,exports,__webpack_require__){"use strict";function supportsAsyncAwait(){try{return eval("async () => {}"),!0}catch(t){return!1}}supportsAsyncAwait()||location.replace("/hodor")}},[1599]);
+//# sourceMappingURL=maps/hodor.map.js
